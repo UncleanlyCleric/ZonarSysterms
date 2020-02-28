fb0a28d284805a9708367384687be8e2e95ce94c
UncleanlyClericr <junyatwin@gmail.com>
2020-02-28 11:30:39 -0800

    Working on the Docker part now

---
+++ myapp/app.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+#pylint: disable = C0200
+'''
+This is a valid language list per region API.  The goal is to implement an
+API that consumes a region from a specific list, and return a list of valid
+languages that exist there.
+
+The data is being parsed from the API at https://restcountries.eu.
+
+The regions to be used are:
+Africa
+Americas
+Asia
+Europe
+Oceania
+
+As there is a large amount of data that comes from that site, there needed to
+be some curating to make sure we're only concerned with the "valid language"
+data.
+
+The way I implemented this, probably isn't the prettiest, because honestly
+this is the first time I've tried to build an API using data from a second
+API, but it /is/ working, so I'll call that one a win.
+'''
+import requests
+from flask import Flask
+
+
+APP = Flask(__name__)
+
+
+def lang_get(region):
+    '''
+    This function queries restfulcounties to grab all countries available.
+    Languages will be parsed out in another function.
+
+    PARAM = region
+    '''
+    base_url = 'https://restcountries.eu/rest/v2/region/'
+    lang_final = []
+    url = base_url + region
+    response = requests.get(url)
+
+    try:
+        if response.status_code == 200:
+            json = response.json()
+            for i in range(0, len(json)):
+                lang_grab = json[i]['languages']
+                lang_work = lang_grab[0]['name']
+                if lang_work not in lang_final:
+                    lang_final.append(lang_work)
+            return lang_final
+        return None
+    except KeyError:
+        return 'Invalid Key'
+
+
+@APP.route('/lang_get/api/<string:region>', methods=['GET'])
+def lang_api(region):
+    '''
+    This is the main API call.
+    '''
+    region_list = ['africa', 'americas', 'asia', 'europe', 'oceania']
+
+    if region not in region_list:
+        error = {'Invalid input: valid domains are': region_list}
+        return error
+    final_list = {region: lang_get(region)}
+    return final_list
+
+
+if __name__ == '__main__':
+    APP.run(debug=True)
